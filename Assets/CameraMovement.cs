518c657d93f60cc0221d61b9e2390f8862a8424d
yliu17 <yliu17@student.unimelb.edu.au>
2017-10-02 02:10:07 +1100

    camera movement

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -3,25 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
-	public GameObject centre;
+	//public GameObject centre;
 	public int speed;
-	private float dot;
+	//private float dot;
+	private Vector3 centrePoint;
 
 
 	// Use this for initialization
 	void Start () {
-		gameObject.transform.position = new Vector3 (0,0,-10);	
+		centrePoint = new Vector3 (4.5f,-0.5f,0.0f);
+		//gameObject.transform.position = new Vector3 (0,0,-10);	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float angle = calculateAngle ();
+		//float angle = calculateAngle ();
 		if (Input.GetAxis ("Mouse X") < 0) {
-			transform.RotateAround (Vector3.zero, Vector3.up, speed * Time.deltaTime);
+			transform.RotateAround (centrePoint, Vector3.up, speed * Time.deltaTime);
 		}else if (Input.GetAxis ("Mouse X") > 0) {
-			transform.RotateAround (Vector3.zero, Vector3.down, speed * Time.deltaTime);
+			transform.RotateAround (centrePoint, Vector3.down, speed * Time.deltaTime);
 		}
-		gameObject.transform.LookAt (centre.transform);
+		//gameObject.transform.LookAt (centre.transform);
 //		else if (Input.GetAxis ("Mouse Y") > 0) {
 //			transform.RotateAround (Vector3.zero, Vector3.left, speed * Time.deltaTime);
 //		}else if (Input.GetAxis ("Mouse Y") > 0) {
@@ -29,18 +31,18 @@ public class CameraMovement : MonoBehaviour {
 //		}
 	}
 
-	float calculateAngle(){
-		Vector3 v1 = gameObject.transform.position;
-		//print (v1);
-		Vector3 v2 = centre.transform.position;
-		//print (v2);
-		dot = Vector3.Dot (v1,v2);
-		print (dot);
-		dot = dot / (v1.magnitude * v2.magnitude);
-
-		float angle = Mathf.Acos (dot) * 180 / Mathf.PI;
-		print (angle);
-		return angle;
-
-	}
+//	float calculateAngle(){
+//		Vector3 v1 = gameObject.transform.position;
+//		//print (v1);
+//		Vector3 v2 = centre.transform.position;
+//		//print (v2);
+//		dot = Vector3.Dot (v1,v2);
+//		print (dot);
+//		dot = dot / (v1.magnitude * v2.magnitude);
+//
+//		float angle = Mathf.Acos (dot) * 180 / Mathf.PI;
+//		print (angle);
+//		return angle;
+//
+//	}
 }
