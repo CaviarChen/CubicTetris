e3f41b8fd5b75cfd1b1249ea1b83b81a023eb577
dodojk <zhet1@student.unimelb.edu.au>
2017-10-08 04:20:16 +1100

    add light movement and improve menu

---
+++ Assets/Game/Scripts/LightMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightMovement : MonoBehaviour {
+
+	public GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.FindGameObjectWithTag ("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (camera.transform.position.z < 0) {
+			if (transform.rotation.x > 0.515) {
+				transform.Rotate (new Vector3(-0.2f, 0, 0));
+			}
+		} else {
+			if (transform.rotation.x < 0.785) {
+				transform.Rotate (new Vector3(0.2f, 0, 0));
+			}
+		}
+	}
+}
---
+++ Assets/Main menu/LoadSceneOnClick.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+	public void LoadByIndex(int sceneIndex) {
+		SceneManager.LoadScene (sceneIndex);
+	}
+}
