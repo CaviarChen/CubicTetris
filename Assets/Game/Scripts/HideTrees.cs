7c11caed585bf57a48bfc41afddffc881158c454
dodojk <zhet1@student.unimelb.edu.au>
2017-10-08 02:12:45 +1100

    add hide trees

---
+++ Assets/Game/Scripts/HideTrees.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideTrees : MonoBehaviour {
+
+	public GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.FindGameObjectWithTag ("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (camera.transform.position.z < 0) {
+			GetComponent<MeshRenderer> ().enabled = true;
+		} else {
+			GetComponent<MeshRenderer> ().enabled = false;
+		}
+	}
+}
